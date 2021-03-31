e9ac44eb570319950758e72a65965eef779f2b80
parkpoomd <parkpoom.d@gmail.com>
2021-03-31 16:42:42 +0700

    Components, Props and Computed Properties

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,37 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+import ThreadShow from '@/components/ThreadShow.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld,
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/thread/:id',
+    name: 'ThreadShow',
+    component: ThreadShow,
+    props: true,
+  },
+  {
+    path: '/about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes,
-});
+})
 
-export default router;
+export default router
