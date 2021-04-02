e3ee75a5f095c0dc6cc58af576e7cf8b6bcdcf0d
parkpoomd <parkpoom.d@gmail.com>
2021-04-02 14:47:22 +0700

    Human Friendly Dates

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -18,14 +18,18 @@
       </div>
     </div>
 
-    <div class="post-date text-faded">
-      {{ post.publishedAt }}
+    <div
+      class="post-date text-faded"
+      :title="post.publishedAt | humanFriendlyDate"
+    >
+      {{ post.publishedAt | diffForHumans }}
     </div>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
+import moment from 'moment'
 
 export default {
   props: {
@@ -41,6 +45,14 @@ export default {
 
     userPostsCount() {
       return Object.keys(this.user.posts).length
+    },
+  },
+  filters: {
+    humanFriendlyDate(date) {
+      return moment.unix(date).format('MMMM Do YYYY, h:mm:ss a')
+    },
+    diffForHumans(date) {
+      return moment.unix(date).fromNow()
     },
   },
 }
