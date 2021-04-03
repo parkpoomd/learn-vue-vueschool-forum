34e09b6841374182bb594d9c5f6cfca7b9d2131a
parkpoomd <parkpoom.d@gmail.com>
2021-04-03 10:19:28 +0700

    Forum Components

---
+++ src/components/ForumList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="forum-list">
+    <h2 class="list-title">
+      <a href="#">Forum</a>
+    </h2>
+
+    <ForumListItem
+      v-for="forum in forums"
+      :key="forum['.key']"
+      :forum="forum"
+    />
+  </div>
+</template>
+
+<script>
+import ForumListItem from './ForumListItem'
+export default {
+  components: {
+    ForumListItem,
+  },
+
+  props: {
+    forums: {
+      required: true,
+      type: Array,
+    },
+  },
+}
+</script>
